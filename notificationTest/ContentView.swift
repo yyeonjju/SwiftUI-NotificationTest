a51717a9839190b9553b7e5067bddf72109da55e
yyeonjju <llilyha123@gmail.com>
2023-07-28 15:46:04 +0900

    Initial Commit

---
+++ notificationTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notificationTest
+//
+//  Created by 하연주 on 2023/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ notificationTest/notificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  notificationTestApp.swift
+//  notificationTest
+//
+//  Created by 하연주 on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct notificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
